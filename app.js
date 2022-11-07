f970ecf1e8e931c70c18dcace1c8396ddf6cb9ce
Kryzztian <cdkittsteiner@yahoo.com>
2022-11-07 15:52:13 -0600

    update until feature 3

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-play(); // This is your main function that runs when the page loads
-
+let secretNumber = 50
 function play(){
-   
+   while(true){
+    let guess = Number(prompt("Guess a Number!"))
+
+    if(guess < secretNumber){
+        alert("Guess Higher");
+    } else if(guess > secretNumber){
+        alert("Guess Lower");
+    } else {
+        alert("Correct!");
+        break;
+    } 
+   }
 }
 
 function playAgain(){
     
 }
+
+
+play(); // This is your main function that runs when the page loads
