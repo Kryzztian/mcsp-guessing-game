3f9352c445743885a70d66fc96f00fb6c668e835
Kryzztian <cdkittsteiner@yahoo.com>
2022-11-07 17:03:38 -0600

    This version has finished feature 3

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-let secretNumber = 50
+const secretNumber = 5 //Math.floor(Math.random() * 10) + 1;
+let count = 0;
+
+
 function play(){
    while(true){
-    let guess = Number(prompt("Guess a Number!"))
-
-    if(guess < secretNumber){
-        alert("Guess Higher");
-    } else if(guess > secretNumber){
+    
+    const guess = prompt("Guess a Number!");
+    const guessNum = Number(guess);
+    if(guess === null){
+        alert("Goodbye!","PeAcE hOmIe")
+        break;
+    }else if(Number.isNaN(guessNum)){
+        alert("Please enter a valid number")
+    } else if(guessNum < secretNumber){
+        count ++;
+        alert("Guess Higher", "Or don't");
+    } else if(guessNum > secretNumber){
+        count ++;
         alert("Guess Lower");
     } else {
-        alert("Correct!");
+            alert(`Correct! It took you ${count} tries!`);
         break;
-    } 
+        }
+    }
    }
-}
 
 function playAgain(){
     
