dd949a50e98435632b64aa058ba9e0b46902498f
Kryzztian <cdkittsteiner@yahoo.com>
2022-11-08 15:52:03 -0600

    All features complete.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+//THIS CODE IS SLOPPY BUT IT WORKS HOW I WANT IT TO
+
 const secretNumber = 5 //Math.floor(Math.random() * 10) + 1;
-let count = 1;
-let arrCount = [];
+let history = {};
+let newName = ""
+
+let hello = prompt('Hello! Please enter your name');
 
 function play(){
-    let hello = prompt('Hello! Please enter your name')
+    let count = 1;
+    
    while(true){
     const guess = prompt("Guess a Number!");
     const guessNum = Number(guess);
-    arrCount.push(guessNum)
-    //figure out how to add a space to this
-    if(guess === null){
+    //arrCount.push(guessNum)
+    //figure out how to add a space to this...
+    //Update, using .join(", ") to join the array into a string with a space inbetween them.
+     if (guess === null){
         alert("Goodbye!","PeAcE hOmIe")
         break;
     }else if(Number.isNaN(guessNum)){
@@ -21,7 +27,26 @@ function play(){
         count ++;
         alert(`Sorry ${hello}, Guess Lower`);
     } else {
-            alert(`Good job ${hello}! It took you ${count} tries! Your guesses were ${arrCount}!`);
+        // first time playing
+        if(/*history[hello] === undefined*/ !history.hasOwnProperty(hello)){
+            alert(`Good job ${hello}! Your guesses were ${count}!`);
+            console.log()
+        } else {
+            // has played before
+            let prevAtt = history[hello];
+            let difference = prevAtt - count;
+            if(difference > 0){
+                alert(`That’s Correct ${hello} And you beat your previous attempt by ${difference} fewer guesses!`)
+            } else if (difference < 0){
+                alert(`That’s Correct ${hello} You did better in your last game by ${Math.abs(difference)} guesses!`)
+            } else {
+                alert(`That’s Correct ${hello} You tied your previous attempt!`)
+            }
+             
+        }
+        history[hello] = count;  
+        console.log(history[hello])
+        console.log(history)
         break;
         }
     }
@@ -29,7 +54,13 @@ function play(){
 
 function playAgain(){
     let choice = prompt("Would you like to play again?")
+    newName = prompt("Please re-enter your name for validation.")
     if( choice.toLowerCase() === "yes"){
+        if(newName == hello){
+            alert(`Welcome back ${hello}. It's time to play again`);
+        } else if(newName !== hello){
+            hello = newName;
+        }
          play()
     } else 
     alert("Thanks for playing!")
